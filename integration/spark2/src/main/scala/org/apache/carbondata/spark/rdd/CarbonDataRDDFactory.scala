1921393f548b65d16cc44ff1d494cfd007365d8f
SangeetaGulia <sangeeta.gulia@knoldus.in>
2017-09-21 14:56:26 +0530

    [CARBONDATA-1827] S3 Carbon Implementation
    1.Provide support for s3 in carbondata.
    2.Added S3Example to create carbon table on s3.
    3.Added S3CSVExample to load carbon table using csv from s3.
    
    This closes #1805

--- integration/spark2/src/main/scala/org/apache/carbondata/spark/rdd/CarbonDataRDDFactory.scala
+++ integration/spark2/src/main/scala/org/apache/carbondata/spark/rdd/CarbonDataRDDFactory.scala
@@ -976,7 +976,8 @@ object CarbonDataRDDFactory {
         sqlContext.sparkContext,
         new DataLoadResultImpl(),
         carbonLoadModel,
-        newRdd
+        newRdd,
+        sqlContext.sparkContext.hadoopConfiguration
       ).collect()
     } catch {
       case ex: Exception =>
